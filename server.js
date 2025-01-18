8fbb5c397117480c894fc4bf38f65d67660e424d
Khanh Do <95413365+dqkhanhcld@users.noreply.github.com>
2025-01-18 16:54:15 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+// Sử dụng Morgan với định dạng 'dev' (phổ biến cho môi trường phát triển)
+app.use(morgan('dev'));
+
 //Hiển thị màn hình 
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    res.send('Hello World!');
 });
 
 //Nghe ngóng 
-app.listen(port, () => {
-    console.log ('Server dang chay tai http://localhost:${port}');
+app.listen(3000, () => {
+    console.log ('Server is listening on port 3000');
 
 });
 
